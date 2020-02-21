8733218a8f4ec872a4c0741c675f45578db854bb
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-02-21 05:12:55 -0700

    Finished

--- player.py
+++ player.py
@@ -1,11 +1,191 @@
+from util import Stack, Queue
+import random
+
+
 class Player:
-    def __init__(self, starting_room):
+    def __init__(self, starting_room, rooms):
+        self.starting_room = starting_room
         self.current_room = starting_room
-    def travel(self, direction, show_rooms = False):
-        next_room = self.current_room.get_room_in_direction(direction)
+        self.graph = {}
+        self.path = []
+        self.rooms = rooms
+        self.moves_forward = 0
+        self.moves_backward = 0
+        self.current_path = []
+        self.room_during_retravel = None
+        self.dead_end_start_rooms = {}
+
+    def travel_long(self, directioins):
+        for direction in directioins:
+            self.travel(direction)
+
+    def travel(self, direction, log_error = True, show_rooms = True):
+        next_room = self.current_room.get_and_remove_room_in_direction(
+            direction, log_error)
         if next_room is not None:
-            self.current_room = next_room
+            self.path.append(direction)
+            self.check_in_graph(next_room)
             if (show_rooms):
-                next_room.print_room_description(self)
+                print(
+                    f"Moving {direction} {self.current_room.id} -> {next_room.id} ")
+            self.current_room = next_room
         else:
             print("You cannot move in that direction.")
+            return False
+
+    def start(self):
+        self.find_dead_ends(self.current_room)
+        print(f"We have found {len(self.dead_end_start_rooms)} dead ends.")
+
+        while len(self.graph) != self.rooms:
+            directions = self.current_room.get_exits()
+            if len(directions) == 0:
+                room = self.bfs_for_next_room(self.current_room)
+                keys = self.dead_end_start_rooms.keys()
+                self.travel_long(room.path)
+                self.dft(self.current_room, True)
+                continue
+            else:
+                next_move = random.choice(directions)
+                room = self.current_room.get_room_in_direction(next_move)
+                keys = self.dead_end_start_rooms.keys()
+                if room.id in keys:
+                    del (self.dead_end_start_rooms[room.id])
+                    print("We are at a dead end thread.")
+                    self.traverse_dead_end()
+                else:
+                    self.travel(next_move, False)
+
+        return [i for i in self.path]
+
+    def traverse_dead_end(self):
+        print("Traversing dead end.")
+        while self.current_room.dead_end_next:
+            keys = self.dead_end_start_rooms.keys()
+            if self.current_room.id in keys:
+                del (self.dead_end_start_rooms[self.current_room.id])
+            direction = self.current_room.dead_end_next_direction
+            if direction is not None:
+                self.current_room.dead_end_next_direction = None
+                self.current_room.dead_end_next = None
+                self.travel(direction)
+            else:
+                break
+
+        print("Finished with dead end.")
+
+    def bfs(self, start, end):
+        queue = Queue()
+        queue.enqueue(start)
+        visited = set()
+
+        while queue.size() > 0:
+            room = queue.dequeue()
+            if room.id not in visited:
+                visited.add(room.id)
+                directions = room.get_exits(False)
+                for direction in directions:
+                    new_room = room.get_room_in_direction(direction)
+                    if new_room.id == end.id:
+                        new_room.path = room.path + [direction]
+                        return new_room
+                    else:
+                        if new_room.id not in visited:
+                            new_room.path = room.path + [direction]
+                            queue.enqueue(new_room)
+        print("We didn't find a path to that room.")
+        return False
+
+    def bfs_for_next_room(self, start):
+        queue = Queue()
+        queue.enqueue(start)
+        visited = set()
+
+        while queue.size() > 0:
+            room = queue.dequeue()
+            if room.id not in visited:
+                visited.add(room.id)
+                directions = room.get_exits(False)
+                for direction in directions:
+
+                    new_room = room.get_room_in_direction(direction)
+                    if len(new_room.unvisited_directions) > 0:
+                        new_room.path = room.path + [direction]
+                        return new_room
+                    else:
+                        if new_room.id not in visited:
+                            new_room.path = room.path + [direction]
+                            queue.enqueue(new_room)
+
+        print("We didn't find a path to a new room.")
+        return False
+
+    def dft(self, start, reset):
+        stack = Stack()
+        stack.push(start)
+        visited = set()
+
+        while stack.size() > 0:
+            room = stack.pop()
+            if reset:
+                room.path = []
+
+            if room.id not in visited:
+                visited.add(room.id)
+                directions = room.get_exits(False)
+                for dir in directions:
+                    next_room = room.get_room_in_direction(dir)
+                    if next_room.id not in visited:
+                        stack.push(next_room)
+
+        print("Graph reset")
+
+    def find_dead_ends(self, start):
+        stack = Stack()
+        stack.push(start)
+        visited = set()
+
+        while stack.size() > 0:
+            room = stack.pop()
+            if room.id not in visited:
+                visited.add(room.id)
+                directions = room.get_exits(False)
+                if len(directions) == 1:
+                    room.dead_end = True
+                    self.find_dead_end_start_room(room)
+                for direction in directions:
+                    new_room = room.get_room_in_direction(direction)
+                    if new_room.id not in visited:
+                        stack.push(new_room)
+        return
+
+    def find_dead_end_start_room(self, start):
+        queue = Queue()
+        queue.enqueue(start)
+        visited = []
+
+        while queue.size() > 0:
+            room = queue.dequeue()
+            if room.id not in visited:
+                visited.append(room.id)
+                directions = room.get_exits(False)
+                if len(directions) > 2:
+                    self.dead_end_start_rooms[room.id] = room
+                    room.is_dead_end_node = True
+                    return
+                else:
+                    for direction in directions:
+                        new_room = room.get_room_in_direction(direction)
+                        if new_room.id not in visited:
+                            new_room.set_dead_end_next(room, direction)
+                            queue.enqueue(new_room)
+
+    def check_in_graph(self, room, print = False):
+        keys = self.graph.keys()
+        if room.id in keys:
+            return
+        else:
+            self.graph[room.id] = room
+            if print:
+                print(f"Visited new room. ID: {room.id}")
+                print(f'{len(self.graph)} found out of {self.rooms}.')
--- room.py
+++ room.py
@@ -1,34 +1,85 @@
+import random
+
 # Implement a class to hold room information. This should have name and
 # description attributes.
+opposites = {"n": "s", "s": "n", "e": "w", "w": "e"}
+
+
 class Room:
-    def __init__(self, name, description, id=0, x=None, y=None):
+    def __init__(self, name, description, id = 0, x = None, y = None):
         self.id = id
         self.name = name
         self.description = description
+        self.unvisited_directions = []
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
         self.x = x
         self.y = y
+        self.weight = 0
+        self.path = []
+        self.dead_end_next_direction = None
+        self.dead_end_prev_direction = None
+        self.dead_end_prev = None
+        self.dead_end_node = None
+        self.dead_end_next = None
+        self.is_dead_end_node = False
+        self.visited_rooms = {}
+
+    def set_dead_end_next(self, next_dead_end_node, direction):
+        # print(
+        #     f"Set dead end next: {self.id}  {opposites[direction]} -> {next_dead_end_node.id}")
+        self.dead_end_next = next_dead_end_node
+        self.dead_end_next_direction = opposites[direction]
+
+    def set_up_dead_end_prev(self, dead_end, direction):
+        # print(
+        #     f"Set dead end prev: {self.id} {direction} -> {dead_end.id}")
+        self.dead_end_prev = dead_end
+        self.dead_end_prev_direction = direction
+
     def __str__(self):
         return f"\n-------------------\n\n{self.name}\n\n   {self.description}\n\n{self.get_exits_string()}\n"
+
     def print_room_description(self, player):
         print(str(self))
-    def get_exits(self):
+
+    def get_exits(self, unvisited_eixts_only = True):
+
         exits = []
-        if self.n_to is not None:
-            exits.append("n")
-        if self.s_to is not None:
-            exits.append("s")
-        if self.w_to is not None:
-            exits.append("w")
-        if self.e_to is not None:
-            exits.append("e")
-        return exits
-    def get_exits_string(self):
-        return f"Exits: [{', '.join(self.get_exits())}]"
+        if not unvisited_eixts_only:
+            if self.n_to is not None:
+                exits.append("n")
+            if self.s_to is not None:
+                exits.append("s")
+            if self.w_to is not None:
+                exits.append("w")
+            if self.e_to is not None:
+                exits.append("e")
+
+            return exits
+
+        else:
+            if len(self.unvisited_directions) == 0:
+                # print("Returning 0 avaiable directions")
+                # print(f"{self.id}")
+                exits = []
+            else:
+                for exit in self.unvisited_directions:
+                    exits.append(exit)
+
+            return exits
+
+    def get_exits_string(self, ):
+        return f"Exits: [{', '.join(self.get_exits(False))}]"
+
     def connect_rooms(self, direction, connecting_room):
+        if direction not in self.unvisited_directions:
+            self.add_unvisited_direction(direction)
+        if opposites[direction] not in connecting_room.unvisited_directions:
+            connecting_room.add_unvisited_direction(opposites[direction])
+
         if direction == "n":
             self.n_to = connecting_room
             connecting_room.s_to = self
@@ -44,6 +95,27 @@ class Room:
         else:
             print("INVALID ROOM CONNECTION")
             return None
+
+    def add_unvisited_direction(self, direction):
+        self.unvisited_directions.append(direction)
+
+    def get_and_remove_room_in_direction(self, direction, log_error):
+        # print(f"Removing route {direction} from {self.id}")
+        room = self.get_room_in_direction(direction)
+
+        not_in = direction not in self.unvisited_directions
+        if not_in:
+            if not log_error:
+                print(
+                    "That room has already been removed from the unvisited directoins")
+                print(f"{self.id} Returning the room anyway. ")
+        else:
+            print(f"Removing {direction} from {self.id}")
+            self.unvisited_directions.remove(direction)
+            self.visited_rooms[direction] = {"Other_id": room.id}
+
+        return room
+
     def get_room_in_direction(self, direction):
         if direction == "n":
             return self.n_to
@@ -55,5 +127,6 @@ class Room:
             return self.w_to
         else:
             return None
+
     def get_coords(self):
         return [self.x, self.y]
